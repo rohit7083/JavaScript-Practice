8258632f312e889934a518ec7203a3ae51796387
MyTution <rohitsonawane889@gmail.com>
2024-07-05 18:55:47 +0530

    Variables

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const  accountId = 14256
+let accEmail = "abc@gmail.com"
+var accPassw="10230"
+let accStatus; //undefined
+// accountId=3
+console.log(accountId);
+console.table([accountId,accEmail,accPassw])
+
+/*
+prefer not to use var 
+because of issue in block scope  and function scope
+*/
