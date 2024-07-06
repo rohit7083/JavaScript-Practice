a444fd89c9dc62b02f4b9efa05a8584ad3fe911f
MyTution <rohitsonawane889@gmail.com>
2024-07-06 12:30:42 +0530

    dataTypes Conversion

---
+++ 03_DatatypeConversion.js
@@ -0,0 +1,28 @@
+let score="father"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNum=99;
+let StringNum = String(someNum)
+console.log(StringNum);
+console.log(typeof StringNum);
